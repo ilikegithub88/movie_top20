7632a2010414ceb95ed3db69352993b83c90a706
ilikegithub88 <tenbillionandmemory@gmail.com>
2020-07-09 19:36:44 +0900

    Updates

---
+++ precache-manifest.f1e02f88a131d82e7cc48154b62e11ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17a83c3b293e4ba1368a6794cdcd2bc",
+    "url": "/movie_top20/index.html"
+  },
+  {
+    "revision": "32ff0dc31d4208e5a330",
+    "url": "/movie_top20/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5951a9b301b2ba74f82f",
+    "url": "/movie_top20/static/css/main.f8192986.chunk.css"
+  },
+  {
+    "revision": "32ff0dc31d4208e5a330",
+    "url": "/movie_top20/static/js/2.80587289.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movie_top20/static/js/2.80587289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5951a9b301b2ba74f82f",
+    "url": "/movie_top20/static/js/main.37de34b7.chunk.js"
+  },
+  {
+    "revision": "9d350d5869fda1078175",
+    "url": "/movie_top20/static/js/runtime-main.31bee5bb.js"
+  }
+]);
